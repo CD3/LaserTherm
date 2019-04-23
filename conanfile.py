8e60392752a018df3e94f6f2824979fef4174340
C.D. Clark III <clifton.clark@gmail.com>
2019-04-23 16:32:46 -0500

    working on conan package

---
+++ conanfile.py
@@ -0,0 +1,27 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class LaserThermConan(ConanFile):
+    name = "LaserTherm"
+    version = "master"
+    license = "MIT"
+    author = "CD Clark III clifton.clark@gmail.com"
+    url = "http:/fermi.fhsu.edu/CD3/LaserTherm"
+    homepage = "http://fermi.fhsu.edu/CD3/LaserTherm"
+    description = "A C++ library for doing laser-tissue simulation.."
+    topics = ("C++", "Laser", "Physics")
+
+    generators = "cmake", "virtualenv"
+    requires = 'boost/1.69.0@conan/stable','eigen/3.3.7@conan/stable'
+    build_requires = 'cmake_installer/3.13.0@conan/stable', 'ninja_installer/1.9.0@bincrafters/stable'
+
+    def build(self):
+
+      cmake = CMake(self)
+
+      # get location of the eigen cmake config file to pass to cmake
+      defs = {}
+      defs["Eigen3_DIR"] = os.path.join( self.deps_cpp_info["eigen"].rootpath, "share", "eigen3", "cmake" )
+      cmake.configure(defs=defs)
+
+      cmake.build()
