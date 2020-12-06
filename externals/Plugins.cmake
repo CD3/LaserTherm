8bafd2a40559bc6768ece73be61a238f621f6364
CD Clark III <clifton.clark@gmail.com>
2020-12-05 22:13:12 -0600

    housekeeping: ran cmake-format

--- externals/Plugins.cmake
+++ externals/Plugins.cmake
@@ -1,5 +1,9 @@
-if( EXISTS ${CMAKE_CURRENT_LIST_DIR}/Format.cmake )
+if(EXISTS ${CMAKE_CURRENT_LIST_DIR}/Format.cmake)
   message(STATUS "Adding Format.cmake")
+  set(FORMAT_CHECK_CMAKE ON)
+  set(CMAKE_FORMAT_EXCLUDE
+      ".*/Format.cmake/.*|.*/Format.cmake"
+      CACHE STRING "CMake formatting file exclusion pattern.")
   add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/Format.cmake)
 else()
   message(STATUS "Format.cmake not found..")
