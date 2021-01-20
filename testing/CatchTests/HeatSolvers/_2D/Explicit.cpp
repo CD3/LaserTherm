4d406b415a5961fd0e4cd1c1dc4e3159701c6886
CD Clark III <clifton.clark@gmail.com>
2021-01-19 20:32:35 -0600

    added unit test skeleton for 2D cylindrical coordinate solver.

---
+++ testing/CatchTests/HeatSolvers/_2D/Explicit.cpp
@@ -0,0 +1,22 @@
+#include "catch.hpp"
+
+#include <fstream>
+#include <iostream>
+
+#include <LaserTherm/HeatSolvers/_2D/Cylindrical/Explicit.hpp>
+
+TEST_CASE("Explicit 2D Cylindrical Heat Solver")
+{
+
+/*     HeatSolvers::_2D::Cylindrical::Explicit<double> HeatSolver(10,20); */
+/*     HeatSolver.T.setCoordinateSystem(Uniform(0, 2),Uniform(0,4)); */
+
+/*     HeatSolver.T.set(1.0); */
+/*     HeatSolver.A.set(2.0); */
+/*     HeatSolver.VHC.set(3.0); */
+/*     HeatSolver.k.set(4.0); */
+
+
+/*     HeatSolver.stepForward(1); */
+
+}
