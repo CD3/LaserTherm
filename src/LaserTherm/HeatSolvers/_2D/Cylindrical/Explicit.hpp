1bb20badef46735ea9d8e553308097a08a455db0
Emily Cranwell <emilycranwell@Emilys-MBP.ad.fhsu.edu>
2021-02-26 17:15:28 -0600

    Added Fields to Explicit class

--- src/LaserTherm/HeatSolvers/_2D/Cylindrical/Explicit.hpp
+++ src/LaserTherm/HeatSolvers/_2D/Cylindrical/Explicit.hpp
@@ -1,12 +1,34 @@
-template <typename N>
+#include <libField/Field.hpp>
+
+template <class N>
 class Explicit{
   public:
-    Explicit(int rj, int zi){
-
+    // constructors
+    Explicit(int _rj, int _zi){
+      this->rj = _rj;
+      this->zi = _zi;
+      T.reset(zi, rj);
+      A.reset(zi, rj);
+      VHC.reset(zi, rj);
+      k.reset(zi, rj);
     }
     Explicit(){
       // Default constructor to be safe
     }
+    // methods
+    void stepForward(N delta_t){
+      // junk goes here wow i spelled everythign right
+    }
+    void A_n(int i, int j){
+
+    }
+    // member variables
+    int rj;
+    int zi;
+    Field<N, 2> T;
+    Field<N, 2> A;
+    Field<N, 2> VHC;
+    Field<N, 2> k;
   protected:
   private:
 };
--- testing/CatchTests/HeatSolvers/_2D/Explicit.cpp
+++ testing/CatchTests/HeatSolvers/_2D/Explicit.cpp
@@ -4,20 +4,22 @@
 #include <iostream>
 
 #include <LaserTherm/HeatSolvers/_2D/Cylindrical/Explicit.hpp>
+using namespace std;
 
 TEST_CASE("Explicit 2D Cylindrical Heat Solver")
 {
 
      //HeatSolvers::_2D::Cylindrical::Explicit<double> HeatSolver(10,20);
     Explicit<double> HeatSolver(10,20);
-/*     HeatSolver.T.setCoordinateSystem(Uniform(0, 2),Uniform(0,4)); */
+    HeatSolver.T.setCoordinateSystem(Uniform(0, 2),Uniform(0,4));
+//      Herbie.destination.address.directions("tomanek");
 
-/*     HeatSolver.T.set(1.0); */
-/*     HeatSolver.A.set(2.0); */
-/*     HeatSolver.VHC.set(3.0); */
-/*     HeatSolver.k.set(4.0); */
+    HeatSolver.T.set(1.0);
+    HeatSolver.A.set(2.0);
+    HeatSolver.VHC.set(3.0);
+    HeatSolver.k.set(4.0);
 
-
-/*     HeatSolver.stepForward(1); */
+    
+    HeatSolver.stepForward(1);
 
 }
