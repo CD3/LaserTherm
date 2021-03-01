0e91432c5b32360446ac534cb90498bb55cfefcf
Emily Cranwell <emilycranwell@Emilys-MBP.ad.fhsu.edu>
2021-03-01 16:21:32 -0600

    Started step forward function in explicit.hpp

--- src/LaserTherm/HeatSolvers/_2D/Cylindrical/Explicit.hpp
+++ src/LaserTherm/HeatSolvers/_2D/Cylindrical/Explicit.hpp
@@ -19,15 +19,39 @@ class Explicit{
       // Default constructor to be safe
     }
     // ---------------------- PUBLIC METHODS -----------------------
+    // delta_t is a step forward in time in seconds
     void stepForward(N delta_t){
-      // junk goes here wow i spelled everythign right
+      N beta;
+      Field<N, 2> T_prime(zi, rj);
+      for(N t = 0.0; t < delta_t; t+=dt){
+        for(int i = 0; i < zi; i++){
+          for(int j = 0; j < rj; j++){
+            beta = delta_t / VHC[i][j];
+            T_prime[i][j] = T[i][j] + beta
+            * (T[i][j] * A_n(i , j)
+            + T[i][j+1] * B_n(i , j)
+            + T[i][j-1] * C_n(i , j)
+            + T[i-1][j] * D_n(i , j)
+            + T[i+1][j] * E_n(i , j));
+          }
+        }
+        T.set(1.0);
+        T *= T_prime;
+      }
+    }
+    int get_rdims(){
+      return this->rj;
+    }
+    int get_zdims(){
+      return this->zi;
     }
     // ----------------- PUBLIC MEMBER VARIABLES -------------------
     Field<N, 2> T;
     Field<N, 2> A;
     Field<N, 2> VHC;
     Field<N, 2> k;
-    
+    double dt = 0.001;
+
   /* Protected namespace is allowed to be
    * accessed by classes that inhereit using
    * public or protected keywords, but not by
@@ -37,11 +61,11 @@ class Explicit{
     // --------------------- PROTECTED METHODS ---------------------
     // Calculate Coefficent for T^n_(r, z)
     N A_n(int i, int j){
-      
+
     }
     N A_nBC(int i, int j){
       // Some reference to Boundary Conditions set elsewhere
-      // double fprime = BC::FiniteDifference<double>::f;
+      // double fprime = BC::FiniteDifference<N>::f;
     }
     // Calculate Coefficent for T^n_(r+1, z)
     N B_n(int i, int j){
@@ -54,7 +78,7 @@ class Explicit{
     N C_n(int i, int j){
 
     }
-    N C_nBC(int i, int j){ 
+    N C_nBC(int i, int j){
     // Some reference to Boundary Conditions set elsewhere
     }
     // Calculate Coefficent for T^n_(r, z-1)
@@ -71,7 +95,7 @@ class Explicit{
     N E_nBC(int i, int j){
       // Some reference to Boundary Conditions set elsewhere
     }
-    
+
 
   private:
     // ---------------------- PUBLIC METHODS -----------------------
