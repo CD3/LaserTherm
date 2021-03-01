e4e4c613ef4c2787f054db3bb7a72402cade64ac
Daniel Huantes <daniel.huantesjr@gmail.com>
2021-03-01 17:32:46 -0600

    Added dkdr, dkdt, A_n, B_n to Explicit.hpp

--- src/LaserTherm/HeatSolvers/_2D/Cylindrical/Explicit.hpp
+++ src/LaserTherm/HeatSolvers/_2D/Cylindrical/Explicit.hpp
@@ -27,6 +27,7 @@ class Explicit{
         for(int i = 0; i < zi; i++){
           for(int j = 0; j < rj; j++){
             beta = delta_t / VHC[i][j];
+            // probably make this a function? vvv
             T_prime[i][j] = T[i][j] + beta
             * (T[i][j] * A_n(i , j)
             + T[i][j+1] * B_n(i , j)
@@ -50,18 +51,37 @@ class Explicit{
     Field<N, 2> A;
     Field<N, 2> VHC;
     Field<N, 2> k;
+    // we (probably) can't explicitly define dt as N type :(
     double dt = 0.001;
 
-  /* Protected namespace is allowed to be
-   * accessed by classes that inhereit using
-   * public or protected keywords, but not by
-   * outside or unrelated classes */
   protected:
     // ---------------- PROTECTED MEMBER VARIABLES  ----------------
     // --------------------- PROTECTED METHODS ---------------------
+    // Finite Difference deriv for kappa wrt r
+    N dk_dr(int i, int j){
+      N numer = (k[i][j + 1] - k[i][j - 1]);
+      // is this an ok way to do 2\Delta r?
+      N denom = k.getCoord(i, j)[1] - k.getCoord(i, j - 1)[1];
+      return numer / denom;
+    }
+    // Finite Difference deriv for kappa wrt z
+    N dk_dz(int i, int j){
+      N numer = (k[i + 1][j] - k[i - 1][j]);
+      // is this an ok way to do 2\Delta r?
+      N denom = k.getCoord(i + 1, j)[0] - k.getCoord(i - 1, j)[0];
+      return numer / denom;
+    }
     // Calculate Coefficent for T^n_(r, z)
     N A_n(int i, int j){
-
+      // k.getCoord returns an array, hopefully like {z, r} to be consistent
+      // so (r, z) is written as (k.getCoord[1], k.getCoord[0])
+      N dr = k.getCoord(i, j + 1)[1] - k.getCoord(i, j)[1];
+      N dz = k.getCoord(i + 1, j)[0] - k.getCoord(i, j)[0];
+      // -2 * kappa / dr**2
+      N T1 = -2 * k[i][j] / pow(dr, 2);
+      // -2 * kappa / dz**2
+      N T2 = -2 * k[i][j] / pow(dz, 2);
+      return T1 + T2;
     }
     N A_nBC(int i, int j){
       // Some reference to Boundary Conditions set elsewhere
@@ -69,28 +89,36 @@ class Explicit{
     }
     // Calculate Coefficent for T^n_(r+1, z)
     N B_n(int i, int j){
-
+      N dr = k.getCoord(i, j + 1)[1] - k.getCoord(i, j)[1];
+      N dz = k.getCoord(i + 1, j)[0] - k.getCoord(i, j)[0];
+      // kappa / 2 r dr
+      N T1 = k[i][j] / (2 * k.getCoord(i, j)[1] * dr);
+      // kappa / dr**2
+      N T2 = k[i][j] / pow(dr, 2);
+      // dk_dr / 2 dr
+      N T3 = dk_dr(i, j) / (2 * dr);
+      return T1 + T2 + T3;
     }
     N B_nBC(int i, int j){
       // Some reference to Boundary Conditions set elsewhere
     }
     // Calculate Coefficent for T^n_(r-1, z)
     N C_n(int i, int j){
-
+      return (N) 0;
     }
     N C_nBC(int i, int j){
     // Some reference to Boundary Conditions set elsewhere
     }
     // Calculate Coefficent for T^n_(r, z-1)
     N D_n(int i, int j){
-
+      return (N) 0;
     }
     N D_nBC(int i, int j){
       // Some reference to Boundary Conditions set elsewhere
     }
     // Calculate Coefficent for T^n_(r, z+1)
     N E_n(int i, int j){
-
+      return (N) 0;
     }
     N E_nBC(int i, int j){
       // Some reference to Boundary Conditions set elsewhere
