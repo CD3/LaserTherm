a70b45a5ec28b21babb49c0a4fa99e5422ebeedb
Daniel Huantes <daniel.huantesjr@gmail.com>
2021-03-09 08:40:22 -0600

    reorganized and created jtest cases in Explicit.cpp

--- testing/CatchTests/HeatSolvers/_2D/Explicit.cpp
+++ testing/CatchTests/HeatSolvers/_2D/Explicit.cpp
@@ -10,48 +10,69 @@ using namespace std;
 
 TEST_CASE("Explicit 2D Cylindrical Heat Solver","[heatsolver]")
 {
+    SECTION("Sandbox testing area"){
+      //HeatSolvers::_2D::Cylindrical::Explicit<double> HeatSolver(10,20);
+      Explicit<double> HeatSolver(10,20);
+      Field<double,2> Tinit(10,20);
+      Tinit.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+      HeatSolver.T.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+      HeatSolver.A.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+      HeatSolver.VHC.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+      HeatSolver.k.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+  //      Herbie.destination.address.directions("tomanek");
+      auto Tlin = [](double z, double r) -> double {
+        return exp(-(pow(r, 2) + pow(z, 2)));
+      };
 
-     //HeatSolvers::_2D::Cylindrical::Explicit<double> HeatSolver(10,20);
-    Explicit<double> HeatSolver(10,20);
-    Field<double,2> Tinit(10,20);
-    Tinit.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
-    HeatSolver.T.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
-    HeatSolver.A.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
-    HeatSolver.VHC.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
-    HeatSolver.k.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
-//      Herbie.destination.address.directions("tomanek");
-    auto Tlin = [](double z, double r) -> double {
-      return exp(-(pow(r, 2) + pow(z, 2)));
-    };
+      HeatSolver.T.set_f([&Tlin] (auto v) -> double {return Tlin(v[0], v[1]);});
+      Tinit.set_f([&Tlin] (auto v) -> double {return Tlin(v[0], v[1]);});
+      HeatSolver.A.set(2.0);
+      HeatSolver.VHC.set(3.0);
+      HeatSolver.k.set(4.0);
 
-    HeatSolver.T.set_f([&Tlin] (auto v) -> double {return Tlin(v[0], v[1]);});
-    Tinit.set_f([&Tlin] (auto v) -> double {return Tlin(v[0], v[1]);});
-    HeatSolver.A.set(2.0);
-    HeatSolver.VHC.set(3.0);
-    HeatSolver.k.set(4.0);
+      {
+        ofstream output;
+        output.open("Tbefore.txt");
+        output << HeatSolver.T;
+        output.close();
+      }
 
-    {
-      ofstream output;
-      output.open("Tbefore.txt");
-      output << HeatSolver.T;
-      output.close();
+      HeatSolver.stepForward(0.05);
+
+      {
+        ofstream output;
+        output.open("Tafter.txt");
+        output << HeatSolver.T;
+        output.close();
+      }
+
+      {
+        ofstream output;
+        output.open("Tdiff.txt");
+        output << Tinit;
+        output.close();
+      }
+    }
+    SECTION("Build Explicit Heat Solver"){
+      // Create and set variables for Explicit. Checking constructors and accessing members
+    }
+    SECTION("Step Forward"){
+      // Check step Forward runs successfully, keeps a constant field constant,
+      // and the center point of a gaussian GOES DOWN!
+    }
+    SECTION("Boundary Conditions"){
+      // Check boundary conditions hold up somehow... what's a good test?
     }
 
-    HeatSolver.stepForward(0.05);
+}
 
-    {
-      ofstream output;
-      output.open("Tafter.txt");
-      output << HeatSolver.T;
-      output.close();
-    }
-
-    {
-      ofstream output;
-      output.open("Tdiff.txt");
-      output << Tinit;
-      output.close();
-    }
+TEST_CASE("Analytic Comparisons"){
+  SECTION("Rod with end at constant temp"){
+  
+  }
+  SECTION("Bessel Function"){
+  
+  }
 }
 
 TEST_CASE("Explicit 2D Cylindrical Heat Solver Validation","[heatsolver][validation]")
