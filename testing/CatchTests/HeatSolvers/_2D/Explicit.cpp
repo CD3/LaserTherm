8d55f4968ac949f9436222f6ef898266bd416afe
Daniel Huantes <daniel.huantesjr@gmail.com>
2021-04-28 15:34:38 -0500

    Added Neumann test section

--- testing/CatchTests/HeatSolvers/_2D/Explicit.cpp
+++ testing/CatchTests/HeatSolvers/_2D/Explicit.cpp
@@ -39,21 +39,23 @@ TEST_CASE("Explicit 2D Cylindrical Heat Solver Validation","[heatsolver][validat
 {
   // see ./doc/writups/Validation/AnalyticalSolutions/AnalyticalSolutions.pdf
   // for a derivation of these tests
-  double R = 2;
-  double L = 4;
   int rN = 141;
   int zN = 282;
-  double dR = R / rN;
-  double dL = L / zN;
+  double R = 2; // m
+  double L = 4; // m
+  double k = 0.5; // W/m/K
+  double rho = 1000; // kg/m^3
+  double c = 4.18; // J/kg/K
+  double dR = R / rN; // m
+  double dL = L / zN; // m
+
   Explicit<double> HeatSolver(zN-2,rN-2);
   Field<double, 2> Aplot(zN,rN);
+
   HeatSolver.T.setCoordinateSystem( Uniform(dL, L-dL), Uniform(dR, R-dR) );
   Aplot.setCoordinateSystem( Uniform(0.,L), Uniform(0., R) );
 
 
-  double k = 0.5; // W/m/K
-  double rho = 1000; // kg/m^3
-  double c = 4.18; // J/kg/K
   HeatSolver.A.set(0.0);
   HeatSolver.VHC.set(rho*c);
   HeatSolver.k.set(k);
@@ -63,6 +65,9 @@ TEST_CASE("Explicit 2D Cylindrical Heat Solver Validation","[heatsolver][validat
 
   SECTION("Dirichlet BC")
   {
+    double dt = 0.001;
+    int Nt = 100;
+
     auto solution = [&](double z, double r, double t)
     {
       double lambda_r = 2.4048/R;
@@ -70,13 +75,11 @@ TEST_CASE("Explicit 2D Cylindrical Heat Solver Validation","[heatsolver][validat
       double alpha = k/rho/c*( lambda_z*lambda_z + lambda_r*lambda_r);
 
       return exp(-alpha*t) * sin(lambda_z*z) * std::cyl_bessel_j(0,lambda_r*r);
-
     };
 
-    double dt = 0.001;
-    int Nt = 100;
     HeatSolver.T.set_f([&](auto x) { return solution(x[0],x[1],0); });
     Aplot.set_f([&](auto x) { return solution(x[0],x[1],Nt*dt); });
+
     for(int i = 0; i < Nt; i++){
       HeatSolver.stepForward(dt);
     }
@@ -94,7 +97,48 @@ TEST_CASE("Explicit 2D Cylindrical Heat Solver Validation","[heatsolver][validat
       output.close();
     }
 
-    /* hdf5write("T_initial.h5", HeatSolver.T); */
+    vector<string> Name;
+    Name.push_back("center");
+    Name.push_back("rmax");
+    Name.push_back("zmax");
+    Name.push_back("zmin");
+    Name.push_back("r=0");
+
+
+    vector<std::pair<int, int>> Points;
+    Points.push_back(std::make_pair<int, int>(zN/2, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, rN-5));
+    Points.push_back(std::make_pair<int, int>(zN-5, rN/2));
+    Points.push_back(std::make_pair<int, int>(3, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, 3));
+
+    for(int i = 0; i < Points.size(); i++){
+      std::pair<int, int> temp = Points[i];
+      std::cout << "Testing " << Name[i] << "\n";
+      CHECK( HeatSolver.T(temp.first, temp.second) == Approx(Aplot(temp.first+1, temp.second+1)).epsilon(0.01));
+    }
+  }
+
+  SECTION("Nuemann BC"){
+    // Neumann is --sad-- happy because his boundary condition test case is ---empty--- full --:(-- :)
+    double dt = 0.001;
+    int Nt = 100;
+
+    auto solution = [&](double z, double r, double t)
+    {
+      double lambda_r = 2.4048/R;
+      double lambda_z = M_PI/L;
+      double alpha = k/rho/c*( lambda_z*lambda_z + lambda_r*lambda_r);
+
+      return exp(-alpha*t) * cos(lambda_z*z) * std::cyl_bessel_j(0,lambda_r*r);
+
+    };
+
+    HeatSolver.T.set_f([&](auto x) { return solution(x[0],x[1],0); });
+    Aplot.set_f([&](auto x) { return solution(x[0],x[1],Nt*dt); });
+    for(int i = 0; i < Nt; i++){
+      HeatSolver.stepForward(dt);
+    }
 
     vector<string> Name;
     Name.push_back("center");
@@ -105,21 +149,17 @@ TEST_CASE("Explicit 2D Cylindrical Heat Solver Validation","[heatsolver][validat
 
 
     vector<std::pair<int, int>> Points;
-    Points.push_back(std::make_pair<int, int>(100, 50));
-    Points.push_back(std::make_pair<int, int>(100, 98-1));
-    Points.push_back(std::make_pair<int, int>(198-1, 50));
-    Points.push_back(std::make_pair<int, int>(1, 50));
-    Points.push_back(std::make_pair<int, int>(100, 1));
+    Points.push_back(std::make_pair<int, int>(zN/2, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, rN-5));
+    Points.push_back(std::make_pair<int, int>(zN-5, rN/2));
+    Points.push_back(std::make_pair<int, int>(3, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, 3));
 
     for(int i = 0; i < Points.size(); i++){
       std::pair<int, int> temp = Points[i];
       std::cout << "Testing " << Name[i] << "\n";
       CHECK( HeatSolver.T(temp.first, temp.second) == Approx(Aplot(temp.first+1, temp.second+1)).epsilon(0.01));
     }
-  }
-
-  SECTION("Nuemann BC"){
-    // Neumann is sad because his boundary condition test case is empty :(
   }
 
   SECTION("Green's Function"){
@@ -142,19 +182,6 @@ TEST_CASE("Explicit 2D Cylindrical Heat Solver Validation","[heatsolver][validat
       HeatSolver.stepForward(dt);
     }
 
-    {
-      ofstream output;
-      output.open("NumericalSol.txt");
-      output << HeatSolver.T;
-      output.close();
-    }
-    {
-      ofstream output;
-      output.open("AnalyticalSol.txt");
-      output << Aplot;
-      output.close();
-    }
-
     vector<string> Name;
     Name.push_back("center");
     Name.push_back("rmax");
@@ -162,14 +189,12 @@ TEST_CASE("Explicit 2D Cylindrical Heat Solver Validation","[heatsolver][validat
     Name.push_back("zmin");
     Name.push_back("r=0");
 
-    CHECK( HeatSolver.T(zN / 2, rN / 2) == Approx(1));
-
     vector<std::pair<int, int>> Points;
-    Points.push_back(std::make_pair<int, int>(100, 50));
-    Points.push_back(std::make_pair<int, int>(100, 98-1));
-    Points.push_back(std::make_pair<int, int>(198-1, 50));
-    Points.push_back(std::make_pair<int, int>(1, 50));
-    Points.push_back(std::make_pair<int, int>(100, 1));
+    Points.push_back(std::make_pair<int, int>(zN/2, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, rN-5));
+    Points.push_back(std::make_pair<int, int>(zN-5, rN/2));
+    Points.push_back(std::make_pair<int, int>(3, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, 3));
 
     for(int i = 0; i < Points.size(); i++){
       std::pair<int, int> temp = Points[i];
