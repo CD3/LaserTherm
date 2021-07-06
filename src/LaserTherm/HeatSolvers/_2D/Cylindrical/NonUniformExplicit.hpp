a54e6d2c028820d8e42b0e4bd0512f25b4a15df0
Daniel Huantes <daniel.huantesjr@gmail.com>
2021-07-06 16:54:34 -0500

    Added test cases for NonUniformExplicit

--- src/LaserTherm/HeatSolvers/_2D/Cylindrical/NonUniformExplicit.hpp
+++ src/LaserTherm/HeatSolvers/_2D/Cylindrical/NonUniformExplicit.hpp
@@ -8,13 +8,24 @@ namespace BC = HeatSolvers::BoundaryConditions;
 namespace FDHS = HeatSolvers::_2D::Cylindrical;
 
 template <class REAL>
-class NonUniformExplicit : public Explicit {
+class NonUniformExplicit : public ExplicitBase<NonUniformExplicit<REAL>, REAL> {
   public:
+    NonUniformExplicit(size_t _zN, size_t _rN){
+      this->rN = _rN;
+      this->zN = _zN;
+      this->T.reset(_zN, _rN);
+      this->A.reset(this->T.getCoordinateSystemPtr());
+      this->VHC.reset(this->T.getCoordinateSystemPtr());
+      this->k.reset(this->T.getCoordinateSystemPtr());
+      this->T_prime.reset(this->T.getCoordinateSystemPtr());
+    }
+    NonUniformExplicit(){
+      // Default constructor to be safe
+    }
   // -------------------- PUBLIC CONSTRUCTORS --------------------
   // 2 existing constructors, Explicit(zN, rN) and default
   // No special constructors yet...
   // ---------------------- PUBLIC METHODS -----------------------
-  // delta_t is a step forward in time in seconds
 
   // functions to overload to make stepForward work as intended (look up if you need to include 'this' keyword to get methods to overwrite correctly?): 
   // get_dr(int, int) (for HeatFlux)
@@ -30,48 +41,77 @@ class NonUniformExplicit : public Explicit {
   }
   // ----------------- PUBLIC MEMBER VARIABLES -------------------
 
-protected:
   // ---------------- PROTECTED MEMBER VARIABLES  ----------------
   // --------------------- PROTECTED METHODS ---------------------
-  // get forward r spacing (backwards at rmax)
-  REAL get_dr(int i, int j){
-    // overwrite here
+  // i -> z index, j -> r index, o -> orientation st o E {-1, 0, 1}
+  REAL get_dr(int i, int j, int o = -1){
     REAL dr = 0.0;
+    if(j == 0){
+      o = 1;
+    } else if(j == this->rN-1) {
+      o = -1;
+    }
+    if(o == -1){
+        dr = this->k.getCoord(i, j)[1] - this->k.getCoord(i, j - 1)[1];
+    } else if (o == 0) {
+        dr = this->k.getCoord(i, j + 1)[1] - this->k.getCoord(i, j - 1)[1];
+    } else if (o == 1) {
+        dr = this->k.getCoord(i, j + 1)[1] - this->k.getCoord(i, j)[1];
+    } 
     return dr;
   }
 
-  // get forward z spacing (backwards at zmax)
-  REAL get_dz(int i, int j){
-    // overwrite here
+  // i -> z index, j -> r index, o -> orientation st o E {-1, 0, 1}
+  REAL get_dz(int i, int j, int o = -1){
     REAL dz = 0.0;
+    if(i == 0){
+      o = 1;
+    } else if(i == this->zN-1) {
+      o = -1;
+    }
+    if(o == -1){
+        dz = this->k.getCoord(i, j)[0] - this->k.getCoord(i - 1, j)[0];
+    } else if (o == 0) {
+        dz = this->k.getCoord(i + 1, j)[0] - this->k.getCoord(i - 1, j)[0];
+    } else if (o == 1) {
+        dz = this->k.getCoord(i + 1, j)[0] - this->k.getCoord(i, j)[0];
+    } 
     return dz;
   }
 
   // see if you wan overwrite all of these FD methods with weighted versions? Check derivation for stretched
-  REAL forward_finite_r(Field<REAL, 1>& f, int i, int j){
-    return f[i][j+1] - f[i][j] / get_dr(i, j);
+  REAL forward_finite_r(Field<REAL, 2>& f, int i, int j){
+    return f[i][j+1] - f[i][j] / get_dr(i, j, 0);
   }
   REAL backward_finite_r(Field<REAL, 2>& f, int i, int j){
-    return f[i][j] - f[i][j-1] / get_dr(i, j);
+    return f[i][j] - f[i][j-1] / get_dr(i, j, 0);
   }
   REAL central_finite_r(Field<REAL, 2>& f, int i, int j){
-    return f[i][j+1] - f[i][j-1] / (2 * get_dr(i,j));
+    REAL C1, C2, C3;
+    C1 = this->get_dr(i, j, -1) / (this->get_dr(i, j, 0) * this->get_dr(i, j, 1));
+    C2 = (this->get_dr(i, j, 1) - this->get_dr(i, j, -1)) / (this->get_dr(i, j, -1) * this->get_dr(i, j, 1));
+    C3 = -1 * this->get_dr(i, j, 1) / (this->get_dr(i, j, 0) * this->get_dr(i, j, -1));
+    return C1 * f[i][j+1] + C2 * f[i][j] + C3 * f[i][j-1];
   }
   REAL forward_finite_z(Field<REAL, 2>& f, int i, int j){
-    return f[i+1][j] - f[i][j] / get_dz(i, j);
+    return f[i+1][j] - f[i][j] / get_dz(i, j, 0);
   }
   REAL backward_finite_z(Field<REAL, 2>& f, int i, int j){
-    return f[i][j] - f[i-1][j] / get_dz(i, j);
+    return f[i][j] - f[i-1][j] / get_dz(i, j, 0);
   }
   REAL central_finite_z(Field<REAL, 2>& f, int i, int j){
-    return f[i+1][j] - f[i-1][j] / (2 * get_dz(i, j));
+    REAL C1, C2, C3;
+    C1 = this->get_dz(i, j, -1) / (this->get_dz(i, j, 0) * this->get_dz(i, j, 1));
+    C2 = (this->get_dz(i, j, 1) - this->get_dz(i, j, -1)) / (this->get_dz(i, j, -1) * this->get_dz(i, j, 1));
+    C3 = -1 * this->get_dz(i, j, 1) / (this->get_dz(i, j, 0) * this->get_dz(i, j, -1));
+    return C1 * f[i+1][j] + C2 * f[i][j] + C3 * f[i-1][j];
   }
   // Finite Difference deriv for kappa wrt r
   REAL dk_dr(int i, int j){
     if (j == 0){
       return forward_finite_r(this->k, i, j);
     }
-    else if (j == rN-1){
+    else if (j == this->rN-1){
       return backward_finite_r(this->k, i, j);
     }
     else{
@@ -83,7 +123,7 @@ protected:
     if (i == 0){
       return forward_finite_z(this->k, i, j);
     }
-    else if (i == zN-1){
+    else if (i == this->zN-1){
       return backward_finite_z(this->k, i, j);
     }
     else{
@@ -92,77 +132,116 @@ protected:
   }
   // Calculate Coefficent for T^n_(z, r)
   REAL A_n(int i, int j){
-    REAL dr = get_dr(i, j);
-    REAL dz = get_dz(i, j);
-    // -2 * kappa / dr**2
-    REAL T1 = -2 * this->k[i][j] / pow(dr, 2);
-    // -2 * kappa / dz**2
-    REAL T2 = -2 * this->k[i][j] / pow(dz, 2);
-    return T1 + T2;
-  }
-  // Calculate Coefficent for T^n_(z, r+1)
-  REAL B_n(int i, int j){
-    REAL dr = get_dr(i, j);
-    REAL dz = get_dz(i, j);
-    // kappa / 2 r dr
-    REAL T1 = this->k[i][j] / (2 * this->k.getCoord(i, j)[1] * dr);
-    // kappa / dr**2
-    REAL T2 = this->k[i][j] / pow(dr, 2);
-    // dk_dr / 2 dr
-    REAL T3 = dk_dr(i, j) / (2 * dr);
-    return T1 + T2 + T3;
+    REAL dr_l, dr_c, dr_r;
+    REAL dz_l, dz_c, dz_r;
+    REAL dk_z, dk_r;
+    REAL r;
+    // the subscripts 'l', 'c', 'r' are left center in place of '-' ' ' '+'
+    dr_l = get_dr(i, j, -1);
+    dr_c = get_dr(i, j, 0);
+    dr_r = get_dr(i, j, 1);
+    // the subscripts 'l', 'c', 'r' are left center in place of '-' ' ' '+'
+    dz_l = get_dz(i, j, -1);
+    dz_c = get_dz(i, j, 0);
+    dz_r = get_dz(i, j, 1);
+    // kappa derivatives
+    dk_z = this->dk_dz(i, j);
+    dk_r = this->dk_dr(i, j);
+
+    r = this->T.getCoord(i, j)[1];
+
+    REAL T1 = (1 / r) * this->k[i][j] * ((dr_r - dr_l) / (dr_r * dr_l));
+    REAL T2 = -2 * this->k[i][j] * (1 / (dr_r * dr_l));
+    REAL T3 = dk_r * ((dr_r - dr_l) / (dr_r * dr_l));
+    REAL T4 = -2 * this->k[i][j] * (1 / (dz_r * dz_l));
+    REAL T5 = dk_z * ((dz_r - dz_l) / (dz_r * dz_l));
+    return T1 + T2 + T3 + T4 + T5;
   }
 
   // Calculate Coefficent for T^n_(z, r-1)
+  REAL B_n(int i, int j){
+    REAL dr_l, dr_c, dr_r;
+    REAL dk_r;
+    REAL r;
+    // the subscripts 'l', 'c', 'r' are left center in place of '-' ' ' '+'
+    dr_l = get_dr(i, j, -1);
+    dr_c = get_dr(i, j, 0);
+    dr_r = get_dr(i, j, 1);
+    // kappa derivatives
+    dk_r = this->dk_dr(i, j);
+
+    r = this->T.getCoord(i, j)[1];
+
+    REAL T1 = (-1.0 / r) * this->k[i][j] * dr_r / (dr_c * dr_l);
+    REAL T2 = (2.0 / (dr_c * dr_l)) * this->k[i][j];
+    REAL T3 = -1.0 * dk_r * dr_r / (dr_c * dr_l);
+    return T1 + T2 + T3;
+  }
+  
+  // Calculate Coefficent for T^n_(z, r+1)
   REAL C_n(int i, int j){
-    //return (N) 0;
-    REAL dr = get_dr(i, j);
-    REAL dz = get_dz(i, j);
-    // - kappa / 2r * dr
-    REAL T1 = - this->k[i][j] / (2 * this->k.getCoord(i, j)[1] * dr);
-    // kappa / dr**2
-    REAL T2 = this->k[i][j] / pow(dr, 2);
-    // dk_dr / 2 dr
-    REAL T3 = - dk_dr(i, j) / (2 * dr);
+    REAL dr_l, dr_c, dr_r;
+    REAL dk_r;
+    REAL r;
+    // the subscripts 'l', 'c', 'r' are left center in place of '-' ' ' '+'
+    dr_l = get_dr(i, j, -1);
+    dr_c = get_dr(i, j, 0);
+    dr_r = get_dr(i, j, 1);
+    // kappa derivatives
+    dk_r = this->dk_dr(i, j);
+
+    r = this->T.getCoord(i, j)[1];
+
+    REAL T1 = (1.0 / r) * this->k[i][j] * dr_l / (dr_c * dr_r);
+    REAL T2 = (2.0 / (dr_c * dr_r)) * this->k[i][j];
+    REAL T3 = dk_r * dr_c / (dr_c * dr_r);
     return T1 + T2 + T3;
   }
 
   // Calculate Coefficent for T^n_(z-1, r)
   REAL D_n(int i, int j){
-    //return (N) 0;
-    REAL dr = get_dr(i, j);
-    REAL dz = get_dz(i, j);
-    // kappa / dz**2
-    REAL T1 = this->k[i][j] / pow(dz, 2);
-    // - dz_dr / 2 dz
-    REAL T2 = - dk_dz(i, j) / (2 * dz);
+    REAL dz_l, dz_c, dz_r;
+    REAL dk_z;
+    // the subscripts 'l', 'c', 'r' are left center in place of '-' ' ' '+'
+    dz_l = get_dz(i, j, -1);
+    dz_c = get_dz(i, j, 0);
+    dz_r = get_dz(i, j, 1);
+    // kappa derivatives
+    dk_z = this->dk_dz(i, j);
+
+    REAL T1 = 2 * this->k[i][j] * (1 / (dz_c * dz_l));
+    REAL T2 = -1 * dk_z * (dz_r / (dz_c * dz_l));
     return T1 + T2;
   }
 
   // Calculate Coefficent for T^n_(z+1, r)
   REAL E_n(int i, int j){
-    //return (N) 0;
-    REAL dr = get_dr(i, j);
-    REAL dz = get_dz(i, j);
-    // kappa / dz**2
-    REAL T1 = this->k[i][j] / pow(dz, 2);
-    // - dz_dr / 2 dz
-    REAL T2 = dk_dz(i, j) / (2 * dz);
+    REAL dz_l, dz_c, dz_r;
+    REAL dk_z;
+    // the subscripts 'l', 'c', 'r' are left center in place of '-' ' ' '+'
+    dz_l = get_dz(i, j, -1);
+    dz_c = get_dz(i, j, 0);
+    dz_r = get_dz(i, j, 1);
+    // kappa derivatives
+    dk_z = this->dk_dz(i, j);
+
+    REAL T1 = 2 * this->k[i][j] * (1 / (dz_c * dz_r));
+    REAL T2 = dk_z * (dz_l / (dz_c * dz_r));
     return T1 + T2;
   }
 
   // Calculate Coefficent for T^n_(z+1, 0)
   REAL A_nR0(int i, int j){
-    REAL dr = get_dr(i, j);
-    REAL dz = get_dz(i, j);
+    REAL dr = get_dr(i, j, 0);
+    REAL dz = get_dz(i, j, 0);
     REAL T1 = (-4 * this->k[i][j]) / (dr * dr);
     REAL T2 = (-2 * this->k[i][j]) / (dz * dz);
     return T1 + T2;
   }
 
   // Calculate Coefficent for T^n_(z+1, 0)
-  REAL B_nR0(int i, int j){
-    REAL dr = get_dr(i, j);
+  REAL C_nR0(int i, int j){
+    REAL dr = get_dr(i, j, 0);
     // 2 * kappa / dr**2
     REAL T1 = 2 * this->k[i][j] / pow(dr, 2);
     // 1 / 2 * dr
@@ -173,8 +252,8 @@ protected:
   }
 
   // Calculate Coefficent for T^n_(z+1, 0)
-  REAL C_nR0(int i, int j){
-    REAL dr = get_dr(i, j);
+  REAL B_nR0(int i, int j){
+    REAL dr = get_dr(i, j, 0);
     // 2 * kappa / dr**2
     REAL T1 = 2 * this->k[i][j] / pow(dr, 2);
     // 1 / 2 * dr
@@ -195,10 +274,8 @@ protected:
   }
 
 
+protected:
 private:
   // ---------------------- PUBLIC METHODS -----------------------
   // ----------------- PRIVATE MEMBER VARIABLES  -----------------
-  Field<REAL, 2> T_prime;
-  int rN;
-  int zN;
 };
--- testing/CatchTests/HeatSolvers/_2D/Explicit.cpp
+++ testing/CatchTests/HeatSolvers/_2D/Explicit.cpp
@@ -6,13 +6,37 @@
 
 //#include <libField/HDF5.hpp>
 #include <LaserTherm/HeatSolvers/_2D/Cylindrical/UniformExplicit.hpp>
+#include <LaserTherm/HeatSolvers/_2D/Cylindrical/NonUniformExplicit.hpp>
 using namespace std;
 
-TEST_CASE("UniformExplicit 2D Cylindrical Heat Solver","[heatsolver]")
+TEST_CASE("Explicit 2D Cylindrical Heat Solver","[heatsolver]")
 {
     SECTION("Build UniformExplicit Heat Solver"){
       //HeatSolvers::_2D::Cylindrical::Explicit<double> HeatSolver(10,20);
       UniformExplicit<double> HeatSolver(20,10);
+      Field<double,2> Tinit(10,20);
+      Tinit.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+      HeatSolver.T.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+      HeatSolver.A.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+      HeatSolver.VHC.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+      HeatSolver.k.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
+  //      Herbie.destination.address.directions("tomanek");
+      auto Tlin = [](double z, double r) -> double {
+        return exp(-(pow(r, 2) + pow(z, 2)));
+      };
+
+      HeatSolver.T.set_f([&Tlin] (auto v) -> double {return Tlin(v[0], v[1]);});
+      Tinit.set_f([&Tlin] (auto v) -> double {return Tlin(v[0], v[1]);});
+      HeatSolver.A.set(2.0);
+      HeatSolver.VHC.set(3.0);
+      HeatSolver.k.set(4.0);
+
+      HeatSolver.stepForward(0.001);
+    }
+
+    SECTION("Build NonUniformExplicit Heat Solver"){
+      //HeatSolvers::_2D::Cylindrical::Explicit<double> HeatSolver(10,20);
+      NonUniformExplicit<double> HeatSolver(20,10);
       Field<double,2> Tinit(10,20);
       Tinit.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
       HeatSolver.T.setCoordinateSystem(Uniform(-2, 2),Uniform(-4,4));
@@ -102,7 +126,7 @@ TEST_CASE("UniformExplicit 2D Cylindrical Heat Solver Validation","[heatsolver][
 
     for(int i = 0; i < Points.size(); i++){
       std::pair<int, int> temp = Points[i];
-      std::cout << "Testing " << Name[i] << "\n";
+      //std::cout << "Testing " << Name[i] << "\n";
       CHECK( HeatSolver.T(temp.first, temp.second) == Approx(Aplot(temp.first+1, temp.second+1)).epsilon(0.01));
     }
   }
@@ -166,7 +190,7 @@ TEST_CASE("UniformExplicit 2D Cylindrical Heat Solver Validation","[heatsolver][
 
     for(int i = 0; i < Points.size(); i++){
       std::pair<int, int> temp = Points[i];
-      std::cout << "Testing " << Name[i] << "\n";
+      //std::cout << "Testing " << Name[i] << "\n";
       CHECK( HeatSolver.T(temp.first, temp.second) == Approx(Aplot(temp.first+1, temp.second+1)).epsilon(0.01));
     }
   }
@@ -210,6 +234,194 @@ TEST_CASE("UniformExplicit 2D Cylindrical Heat Solver Validation","[heatsolver][
     for(int i = 0; i < Points.size(); i++){
       std::pair<int, int> temp = Points[i];
       std::cout << "Testing " << Name[i] << "\n";
+      //CHECK( HeatSolver.T(temp.first, temp.second) == Approx(Aplot(temp.first+1, temp.second+1)).epsilon(0.01));
+    }
+
+  }
+}
+
+TEST_CASE("NonUniformExplicit 2D Cylindrical Heat Solver Validation","[heatsolver][validation]")
+{
+  // see ./doc/writups/Validation/AnalyticalSolutions/AnalyticalSolutions.pdf
+  // for a derivation of these tests
+  int rN = 100;
+  int zN = 200;
+  double R = 2; // m
+  double L = 4; // m
+  double k = 0.5; // W/m/K
+  double rho = 1000; // kg/m^3
+  double c = 4.18; // J/kg/K
+  double dR = R / rN; // m
+  double dL = L / zN; // m
+
+  NonUniformExplicit<double> HeatSolver(zN-2,rN-2);
+  Field<double, 2> Aplot(zN,rN);
+
+  HeatSolver.T.setCoordinateSystem( Geometric(0., 0.0117, 1.01), Geometric(0., 0.00633, 1.01) );
+  Aplot.setCoordinateSystem( Geometric(0., 0.0117, 1.01), Geometric(0., 0.00633, 1.01) );
+
+
+  HeatSolver.A.set(0.0);
+  HeatSolver.VHC.set(rho*c);
+  HeatSolver.k.set(k);
+  HeatSolver.minZBC.f = 0;
+  HeatSolver.maxZBC.f = 0;
+  HeatSolver.maxRBC.f = 0;
+
+  SECTION("Dirichlet BC")
+  {
+    double dt = 0.001;
+    int Nt = 100;
+
+    auto solution = [&](double z, double r, double t)
+    {
+      double lambda_r = 2.4048/R;
+      double lambda_z = M_PI/L;
+      double alpha = k/rho/c*( lambda_z*lambda_z + lambda_r*lambda_r);
+
+      return exp(-alpha*t) * sin(lambda_z*z) * std::cyl_bessel_j(0,lambda_r*r);
+    };
+
+    HeatSolver.T.set_f([&](auto x) { return solution(x[0],x[1],0); });
+    Aplot.set_f([&](auto x) { return solution(x[0],x[1],Nt*dt); });
+
+    for(int i = 0; i < Nt; i++){
+      HeatSolver.stepForward(dt);
+    }
+
+
+    {
+      ofstream output;
+      output.open("geometricPlot.txt");
+      output << HeatSolver.T;
+      output.close();
+    }
+    
+    vector<string> Name;
+    Name.push_back("center");
+    Name.push_back("rmax");
+    Name.push_back("zmax");
+    Name.push_back("zmin");
+    Name.push_back("r=0");
+
+
+    vector<std::pair<int, int>> Points;
+    Points.push_back(std::make_pair<int, int>(zN/2, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, rN-5));
+    Points.push_back(std::make_pair<int, int>(zN-5, rN/2));
+    Points.push_back(std::make_pair<int, int>(3, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, 3));
+
+    for(int i = 0; i < Points.size(); i++){
+      std::pair<int, int> temp = Points[i];
+      //std::cout << "Testing " << Name[i] << "\n";
+      CHECK( HeatSolver.T(temp.first, temp.second) == Approx(Aplot(temp.first, temp.second)).epsilon(0.01));
+    }
+  }
+
+  SECTION("Nuemann BC"){
+    // Neumann is --sad-- happy because his boundary condition test case is ---empty--- full --:(-- :)
+    double dt = 0.001;
+    int Nt = 100;
+
+    HeatSolver.minZBC.type = BC::Type::HeatFlux;
+    HeatSolver.maxZBC.type = BC::Type::HeatFlux;
+    HeatSolver.maxRBC.type = BC::Type::HeatFlux;
+    HeatSolver.minZBC.f = 0;
+    HeatSolver.maxZBC.f = 0;
+    HeatSolver.maxRBC.f = 0;
+
+    auto solution = [&](double z, double r, double t)
+    {
+      double lambda_r = 3.8317/R;
+      double lambda_z = M_PI/L;
+      double alpha = k/rho/c*( lambda_z*lambda_z + lambda_r*lambda_r);
+
+      return exp(-alpha*t) * cos(lambda_z*z) * std::cyl_bessel_j(0,lambda_r*r);
+    };
+
+    HeatSolver.T.set_f([&](auto x) { return solution(x[0],x[1],0); });
+    Aplot.set_f([&](auto x) { return solution(x[0],x[1],Nt*dt); });
+    for(int i = 0; i < Nt / 2; i++){
+      HeatSolver.stepForward(dt);
+    }
+    for(int i = Nt / 2; i < Nt; i++){
+      HeatSolver.stepForward(dt);
+    }
+    {
+      ofstream output;
+      output.open("T1.txt");
+      output << HeatSolver.T;
+      output.close();
+    }
+    {
+      ofstream output;
+      output.open("T2.txt");
+      output << Aplot;
+      output.close();
+    }
+
+    vector<string> Name;
+    Name.push_back("center");
+    Name.push_back("rmax");
+    Name.push_back("zmax");
+    Name.push_back("zmin");
+    Name.push_back("r=0");
+
+
+    vector<std::pair<int, int>> Points;
+    Points.push_back(std::make_pair<int, int>(zN/2, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, rN-5));
+    Points.push_back(std::make_pair<int, int>(zN-5, rN/2));
+    Points.push_back(std::make_pair<int, int>(3, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, 3));
+
+    for(int i = 0; i < Points.size(); i++){
+      std::pair<int, int> temp = Points[i];
+      //std::cout << "Testing " << Name[i] << "\n";
+      CHECK( HeatSolver.T(temp.first, temp.second) == Approx(Aplot(temp.first, temp.second)).epsilon(0.01));
+    }
+  }
+
+  SECTION("Green's Function"){
+    // Remember: reset values of HeatSolver(/aplot)
+    auto solution = [&](double z, double r, double t)
+    {
+      return 0;
+
+    };
+
+    double dt = 0.001;
+    int Nt = 100;
+
+    HeatSolver.T.set(0);
+    Aplot.set_f([&](auto x) { return solution(x[0],x[1],Nt*dt); });
+
+    HeatSolver.A[zN / 2][rN / 2] = 1;
+
+    for(int i = 0; i < Nt / 2; i++){
+      HeatSolver.stepForward(dt);
+    }
+
+
+
+    vector<string> Name;
+    Name.push_back("center");
+    Name.push_back("rmax");
+    Name.push_back("zmax");
+    Name.push_back("zmin");
+    Name.push_back("r=0");
+
+    vector<std::pair<int, int>> Points;
+    Points.push_back(std::make_pair<int, int>(zN/2, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, rN-5));
+    Points.push_back(std::make_pair<int, int>(zN-5, rN/2));
+    Points.push_back(std::make_pair<int, int>(3, rN/2));
+    Points.push_back(std::make_pair<int, int>(zN/2, 3));
+
+    for(int i = 0; i < Points.size(); i++){
+      std::pair<int, int> temp = Points[i];
+      //std::cout << "Testing " << Name[i] << "\n";
       //CHECK( HeatSolver.T(temp.first, temp.second) == Approx(Aplot(temp.first+1, temp.second+1)).epsilon(0.01));
     }
 
